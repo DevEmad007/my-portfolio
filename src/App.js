f5f95d383e633ad7aae8622710244b324d096054
DevEmad007 <farshidibtihaj@gmail.com>
2023-08-11 21:34:33 +0600

    router error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import React from 'react'
 import './App.css';
+// import * as ReactDOM from "react-dom/client";
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-
-
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>its checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>login</h1>
+          </Route>
+          <Route path="/">
+            <h1>Home</h1>
+          </Route>
+        </Switch>
+      </div> 
+    </Router>
   );
 }
 
